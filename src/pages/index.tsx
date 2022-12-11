9a4ec7d46dd4a02e3cccb7c7445455340ac315de
Rahulbhola9034 <Rahulbhola9034@gmail.com>
2022-12-11 17:48:29 +0530

    configured all tools

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+      <Typography variant="h1">hello world</Typography>
+    </>
+  );
+}
